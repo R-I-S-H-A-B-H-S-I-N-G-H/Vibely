828969f439b9c3c8a28e12121340c61b4d5d3518
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2025-05-11 20:54:05 +0530

    added to env

--- api/database-config/database.go
+++ api/database-config/database.go
@@ -1,6 +1,8 @@
 package databaseconfig
 
 import (
+	"os"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,7 +12,7 @@ type Database struct{}
 var MY_SQL_DB *gorm.DB
 
 func (d *Database) Init() (*gorm.DB, error) {
-	dsn := "root:lemein..23@tcp(127.0.0.1:3306)/vibely?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := os.Getenv("MYSQL_DSN")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	MY_SQL_DB = db
 	return db, err
